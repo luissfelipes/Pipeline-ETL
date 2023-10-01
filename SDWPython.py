855e662d7b981e6822c8452c4595c0bbe960a686
luissfelipes <luisfelipeww@gmail.com>
2023-10-01 20:10:38 -0300

    Inicio transformação

--- SDWPython.py
+++ SDWPython.py
@@ -1,3 +1,6 @@
+# Pipeline de ETL 
+
+# Extração 
 
 sdw2023_api_url = 'https://sdw-2023-prd.up.railway.app'
 
@@ -19,3 +22,27 @@ def get_user(id):
 users = [user for id in user_ids if (user := get_user(id)) is not None] # operador aurus := permite atribuir em formato de expressão
 
 print(json.dumps(users, indent=2))
+
+# Até aqui foi o processo de extração de dados da tabela
+
+#########################################################################
+
+# Transformação com a API do ChatGPT
+
+
+openai_api_key = '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' #atribuição da chave de API da openai 
+
+import openai
+
+openai.api_key = openai_api_key # Chave de API da openai 
+
+# função que criou requisição de chat completion que conversa com o chatgpt
+
+def generate_ai_news(user):
+    completion = openai.ChatCompletion.create( 
+        model="gpt-3.5-turbo",
+        messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Hello!"}
+    ]
+    )
