ac579fc24f05717b49560d57c20783da10fa837b
luissfelipes <luisfelipeww@gmail.com>
2023-10-05 11:45:22 -0300

    Codigo DIO

--- SDWPython.py
+++ SDWPython.py
@@ -21,7 +21,7 @@ def get_user(id):
 
 users = [user for id in user_ids if (user := get_user(id)) is not None] # operador aurus := permite atribuir em formato de expressão
 
-print(json.dumps(users, indent=2))
+# print(json.dumps(users, indent=2))
 
 # Até aqui foi o processo de extração de dados da tabela
 
@@ -30,7 +30,7 @@ print(json.dumps(users, indent=2))
 # Transformação com a API do ChatGPT
 
 
-openai_api_key = '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' #atribuição da chave de API da openai 
+openai_api_key = '' #Adicionar aqui a atribuição da chave de API da openai 
 
 import openai
 
@@ -42,7 +42,37 @@ def generate_ai_news(user):
     completion = openai.ChatCompletion.create( 
         model="gpt-3.5-turbo",
         messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Hello!"}
-    ]
+            {   
+                "role": "system", 
+                "content": "VocÊ é um especialista em marketing bancário."
+            },
+            {   
+                "role": "user", 
+                "content": f"Crie uma mensagem para {user['name']} sobre a importância dos investimentos (máximo de 100 caracteres)"
+            }
+        ]
     )
+
+    return completion.choices[0].message.content.strip('\"')
+
+for user in users:
+    news = generate_ai_news(user)
+    print(news)
+    user['news'].append({
+        "icon": "https://digitalinnovationone.github.io/santander-dev-week-2023-api/icons/credit.svg",
+        "description": news
+    })
+
+
+####################################################################################
+
+# Começa a parte de load, carregamento das informações para a API da SDW23
+
+def update_user(user):
+    response = requests.put(f"{sdw2023_api_url}/user/{user['id']}", json=user)
+    return True if response.status_code == 200 else False
+
+for user in users: 
+    success = update_user(user)
+    print(f"User{user['name']} updated? {success}!")
+
